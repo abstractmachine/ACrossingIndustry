371529e5e0f5d3e1fec2649796a18f84584115c9
abstractmachine <douglas@abstractmachine.net>
2014-07-15 14:56:13 +0200

    Highway repairs

---
+++ Assets/Scripts/Robots/Barriere.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Barriere : MonoBehaviour {
+
+		public Material transparentMaterial;
+		public Material opaqueMaterial;
+		
+		GameObject barbelee;
+		
+		void Start() {
+		
+				barbelee = transform.Find("Barbelee").gameObject;
+		
+		}
+
+
+		void OnTriggerEnter(Collider other) {
+				print(other);
+				if (other.gameObject.name == "Ouvrier") {
+						//renderer.material = opaqueMaterial;
+						barbelee.SetActive(true);
+						barbelee.SetActiveRecursively(true);
+				}
+		}
+
+		void OnTriggerExit(Collider other) {
+				if (other.gameObject.name == "Ouvrier") {
+						//renderer.material = transparentMaterial;
+						barbelee.SetActive(false);
+						barbelee.SetActiveRecursively(false);
+				}
+		}
+
+}
